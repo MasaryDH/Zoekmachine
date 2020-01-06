43a8004dfec2cd01f4971574ef9a8628b3337029
MasaryDH <sam.deherdt@gmail.com>
2020-01-06 02:38:20 +0100

    Basic layout, Working searchbar, Radius caluculation, Show 5 nearby offices

--- src/Controller/ZoekmachineController.php
+++ src/Controller/ZoekmachineController.php
@@ -3,48 +3,94 @@
 namespace App\Controller;
 
 use App\Entity\Offices;
+use Psr\Container\ContainerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class ZoekmachineController extends AbstractController {
-    /**
-     * @Route("/")
-     */
-    public function homepage(){
-        $number = random_int(0, 100);
-        return new Response('
-        <html>
-            <body>
-                <div>
-                    <p>Show offices in the neighborhood of:</p>
-                    <input name="testInputField"><br>
-                    <input type="checkbox" name="checkbox1">
-                    <input type="checkbox" name="checkbox2"><br>
-                    <textarea></textarea>
-                    Lucky number: '.$number.'
-                </div>
-            </body>
-        </html>');
+
+    private function sortOffices($searchedCity){
+        // DATABANK OPHALEN
+        $repository = $this->getDoctrine()->getRepository(Offices::class);
+        $offices = $repository->findAll();
+
+
+        // STEDEN
+        switch($searchedCity) {
+            case "Gent":
+                $inputLat = 51.054633;
+                $inputLong = 3.7197544;
+                break;
+            case "Brugge":
+                $inputLat = 51.2093205;
+                $inputLong = 3.2220239;
+                break;
+            case "Antwerpen":
+                $inputLat = 51.2171919;
+                $inputLong = 4.4192574;
+                break;
+            case "Brussel":
+                $inputLat = 50.858638;
+                $inputLong = 4.353416;
+                break;
+            case "Kortrijk":
+                $inputLat = 50.808208;
+                $inputLong = 3.266394;
+                break;
+            case "Hasselt":
+                $inputLat = 50.937960;
+                $inputLong = 5.306940;
+                break;
+            case "":
+                $inputLat = 0;
+                $inputLong = 0;
+                break;
+        }
+
+//      RADIUS BEREKENEN
+
+//        $get_data = file_get_contents('http://api.geoname.org/search?q=gent&country=belgium');
+//        $response = json_decode($get_data, true);
+//        var_dump($response);
+        $radiusArray = array();
+        foreach($offices as $office) {
+            $radius = pow((pow(($office->getLongitude()-$inputLong),2) + pow(($office->getLatitude()-$inputLat),2)), 0.5);
+            $radiusArray[$office->getId()] = $radius;
+        }
+
+        asort($radiusArray);
+        $sortedOffices = array_slice($radiusArray, 0, 5, true);
+        $sortedAdresses = array();
+        foreach($sortedOffices as $key => $value){
+            foreach($offices as $office) {
+                if ($office->getId() == $key) {
+                    $officeString= "'".$office->getStreet().",".$office->getCity()."'";
+                    array_push($sortedAdresses, $officeString);
+                    break;
+                }
+            }
+        }
+        return $sortedAdresses;
     }
 
     /**
-     * @Route("/zoekmachine/{slug}")
+     * @Route("/zoekmachine", methods={"POST, GET"})
+     * @param $request
+     * @return Response
      */
-    public function show($slug){
-        $repository = $this->getDoctrine()->getRepository(Offices::class);
-
-        $offices = $repository->findAll();
-        
-//        // Specific search trough database
-//        $offices = $repository->findBy([
-//            'city' => 'GENT',
-//        ]);
-
-        return $this->render('zoekmachine/show.html.twig', [
-            'title' => ucwords(str_replace('-','',$slug)),
-            'offices' => $offices,
-        ]);
+    public function show(Request $request){
+        if ($request->isXmlHttpRequest()) {
+            $test1 = $request->request->get('searchOffice');
+            $test2 = json_encode($this->sortOffices($test1));
+            return new JsonResponse($test2);
+        } else {
+            return $this->render('zoekmachine/show.html.twig', [
+                'title' => 'Offices search',
+            ]);
+        }
     }
 }
